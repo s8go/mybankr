6edf291a2f5bbf1ceaa472efb7db9d654e2f2e37
segun <soshice@gmail.com>
2022-12-15 17:53:48 +0100

    index removal

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import style from "./styles/css/App.css";
+// import style from "./styles/css/App.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(style)
+// console.log(style)
 root.render(
   <React.StrictMode>
     <App />
